7194a65ca1872c1317f2b2d1595e273e1e6e5844
william swanson & adam won & filipe silvestre <student@epicodus.com>
2015-08-18 12:25:03 -0700

    did quite a bit of work here

---
+++ tests/TaskTest.php
@@ -0,0 +1,91 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+    require_once "src/Task.php";
+    require_once "src/Category.php";
+    $server = 'mysql:host=localhost;dbname=to_do_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+    class TaskTest extends PHPUnit_Framework_TestCase
+    {
+          protected function tearDown()
+          {
+                Task::deleteAll();
+                Category::deleteAll();
+          }
+          function test_save()
+          {
+              //Arrange
+              $description = "Wash the dog";
+              $category_id = null;
+              $test_Task = new Task($description, $category_id);
+              //Act
+              $test_Task->save();
+              //Assert
+              $result = Task::getALL();
+              $this->assertEquals($test_Task, $result[0]);
+          }//end function
+          function test_getAll()
+          {
+              //Arrange
+              $description = "Wash the dog";
+              $description2 = "Water the lawn";
+              $category_id = null;
+              $test_Task = new Task($description, $category_id);
+              $test_Task->save();
+              $test_Task2 = new Task($description2, $category_id);
+              $test_Task2->save();
+              //Act
+              $result = Task::getALL();
+              //Assert
+              $this->assertEquals([$test_Task,$test_Task2], $result);
+          }//end function
+          function test_deleteAll()
+          {
+              //Arrange
+              $description = "Wash the dog";
+              $description2 = "Water the lawn";
+              $category_id = null;
+              $test_Task = new Task($description, $category_id);
+              $test_Task->save();
+              $test_Task2 = new Task($description2, $category_id);
+              $test_Task2->save();
+              //Act
+              Task::deleteAll();
+              //Assert
+              $result = Task::getAll();
+              $this->assertEquals([], $result);
+          }//end function
+          function test_getId()
+          {
+              //Arrange
+              $description = "Wash the dog";
+              $id = 1;
+              $category_id = null;
+              $test_Task = new Task($description, $id, $category_id);
+              //Act
+              $result = $test_Task->getId();
+              //Assert
+              $this->assertEquals(1, $result);
+          }
+         function test_find()
+         {
+             //Arrange
+                $description = "Wash the dog";
+                $description2 = "Water the lawn";
+                $category_id = null;
+                $test_Task = new Task($description, $category_id);
+                $test_Task->save();
+                $test_Task2 = new Task($description2, $category_id);
+                $test_Task2->save();
+            //Act
+                $id = $test_Task->getId();
+                $result = Task::find($id);
+            //Assert
+                $this->assertEquals($test_Task, $result);
+         }
+    }// end class
+?>
